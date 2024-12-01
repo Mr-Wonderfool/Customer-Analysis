d6ff75f5b3b0c415753384682301cf4e3792c9ca
Mr-Wonderfool <2251804@tongji.edu.cn>
2024-12-01 16:09:01 +0800

    feat: feature engineering

--- models/Hierarchical/clustering.py
+++ models/Hierarchical/clustering.py
@@ -1,8 +1,9 @@
-from models.utils.feature_engineering import load_data
+from models.utils.feature_engineering import load_data, standard_scale
 
 def main():
     data = load_data("../../data")
     print(f"number of features extracted: {len(data.columns)}")
+    scaled_data = standard_scale(data)
 
 if __name__ == "__main__":
     main()
