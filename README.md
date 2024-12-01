8360bfbca60f458068486da05adf9c01e872189b
Mr-Wonderfool <2251804@tongji.edu.cn>
2024-12-01 23:34:14 +0800

    feat: update project

--- README.md
+++ README.md
@@ -9,3 +9,10 @@ chmod +x config.sh
 # windows程序直接运行脚本，请确保安装了git bash或其他bash工具
 ./config.sh
 ```
+
+### 文件说明
+在`models`文件夹下：
+- `Density`中储存了基于密度聚类的文件，`density_clustering.ipynb`中调库实现，并储存了运行结果；`density_model.py`中采用手写实现DBSCAN。
+- `Hierarchical`中储存了层次聚类相关文件，运行`clustering.py`将聚类结果和分析结果储存在`images`文件夹下。
+- `KMeans`中`KMeans.py`运行得到手写Kmeans和调库实现的聚类结果。
+- `utils`中储存了辅助文件，包括数据处理和可视化。
--- models/Hierarchical/clustering.py
+++ models/Hierarchical/clustering.py
@@ -1,9 +1,30 @@
 from models.utils.feature_engineering import load_data, standard_scale
+from sklearn.decomposition import PCA
+from sklearn.cluster import AgglomerativeClustering
+import matplotlib.pyplot as plt
+from models.utils.visualization import Plotter
+import seaborn as sns
+plt.style.use("ggplot")
+palette = Plotter._PALETTE_
 
 def main():
     data = load_data("../../data")
-    print(f"number of features extracted: {len(data.columns)}")
-    scaled_data = standard_scale(data)
+    data_scaled = standard_scale(data)
+    pca = PCA(n_components=3)
+    pca.fit(data_scaled)
+    data_pca = pca.transform(data_scaled)
+    cluster = AgglomerativeClustering(n_clusters=4, )
+    yhat = cluster.fit_predict(data_pca)
+    data["Clusters"] = yhat
+    plt.figure(figsize=(10,8))
+    ax = plt.subplot(111, projection='3d', label="bla")
+    ax.scatter(data_pca[..., 0], data_pca[..., 1], data_pca[..., 2], s=40, c=yhat, marker='o', cmap='Blues')
+    ax.set_title("Hierarchical Clustering Visualization")
+    plt.savefig("../../images/hierarchical_clustering.png", dpi=150)
+    pl = sns.scatterplot(data = data, x=data["Spent"], y=data["Income"], hue=data["Clusters"], palette=palette)
+    pl.set_title("Cluster's Profile Based On Income And Spending")
+    plt.legend()
+    plt.savefig("../../images/cluster_profile_spent_income.png", dpi=150)
 
 if __name__ == "__main__":
     main()
